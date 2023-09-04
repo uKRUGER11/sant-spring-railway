b9ed929375939852a1fbf8dc70245b26fc728a41
ukruger11 <krugergabriel27@outlook.com>
2023-09-04 14:08:07 -0300

    Project created

---
+++ src/main/java/me/dio/sant/SantApplication.java
@@ -0,0 +1,13 @@
+package me.dio.sant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SantApplication.class, args);
+	}
+
+}
