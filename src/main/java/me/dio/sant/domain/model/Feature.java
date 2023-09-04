a049153cc8a8555fbc7ff6f10727440c8536321e
ukruger11 <krugergabriel27@outlook.com>
2023-09-04 15:17:22 -0300

    Implementação da camada de entidades e ORM

---
+++ src/main/java/me/dio/sant/domain/model/Feature.java
@@ -0,0 +1,9 @@
+package me.dio.sant.domain.model;
+
+import com.fasterxml.jackson.databind.ser.Serializers;
+import jakarta.persistence.Entity;
+
+@Entity(name = "tb_feature")
+public class Feature extends BaseItem {
+
+}
---
+++ src/main/java/me/dio/sant/domain/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package me.dio.sant.domain.repository;
+
+import me.dio.sant.domain.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+}
